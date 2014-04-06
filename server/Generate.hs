a44afe490b5f9ca8dd8c63b944b6be303701a0d5
Simon Buist <simon.buist@gmail.com>
2014-04-06 19:17:58 +0100

    Determine whether a compilation succeeded or failed by returning a (H.Html, Maybe String) (where String is the error)

--- server/Generate.hs
+++ server/Generate.hs
@@ -10,6 +10,47 @@ import qualified Text.Blaze.Html5.Attributes as A
 import qualified Elm.Internal.Utils as Elm
 import Utils
 
+newHtml :: String -> String -> (H.Html, Maybe String)
+newHtml name src =
+    let elmname = "Elm." ++ fromMaybe "Main" (Elm.moduleName src) 
+    in
+      case Elm.compile src of
+          Right jsSrc -> do
+              (getHtmlPage name elmname jsSrc, Nothing)
+          Left err -> do
+              (getErrPage name err, Just err)
+
+
+getHtmlPage :: String -> String -> String -> H.Html
+getHtmlPage name elmname jsSrc =
+  H.docTypeHtml $ do
+      H.head $ do
+        H.meta ! A.charset "UTF-8"
+        H.title . H.toHtml $ name
+        H.style ! A.type_ "text/css" $ preEscapedToMarkup
+         ("a:link {text-decoration: none; color: rgb(15,102,230);}\n\
+          \a:visited {text-decoration: none}\n\
+          \a:active {text-decoration: none}\n\
+          \a:hover {text-decoration: underline; color: rgb(234,21,122);}" :: String)
+      H.body $ do
+        let js = H.script ! A.type_ "text/javascript"
+            runFullscreen = "var runningElmModule = Elm.fullscreen(" ++ elmname ++ ")"
+        js ! A.src (H.toValue ("/elm-runtime.js?0.11" :: String)) $ ""
+        js $ preEscapedToMarkup jsSrc
+        js $ preEscapedToMarkup runFullscreen
+
+getErrPage :: String -> String -> H.Html
+getErrPage name err =
+  H.docTypeHtml $ do
+      H.head $ do
+        H.meta ! A.charset "UTF-8"
+        H.title . H.toHtml $ name
+      H.body $
+        H.span ! A.style "font-family: monospace;" $
+        mapM_ (\line -> preEscapedToMarkup (addSpaces line) >> H.br) (lines err)
+    
+            
+
 -- | Using a page title and the full source of an Elm program, compile down to
 --   a valid HTML document.
 html :: String -> String -> H.Html
