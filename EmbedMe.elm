663224b05d142620868c749e5bd85a75d98d9afd
Simon Buist <simon.buist@gmail.com>
2014-04-11 02:42:59 +0100

    Add timestamps

--- EmbedMe.elm
+++ EmbedMe.elm
@@ -11,19 +11,18 @@ import Json
 (~>) = flip lift
 infixl 4 ~>
 
-clicks : Signal (Int,Int)
-clicks = sampleOn Mouse.isDown Mouse.position
+clicks : Signal (Time, (Int,Int))
+clicks = timestamp (sampleOn Mouse.isDown Mouse.position)
 
 
 firebaseRequest requestType requestData = Http.request requestType "https://sweltering-fire-9141.firebaseio.com/dissertation.json" requestData []
-
  
 serialize r = r |> JEXP.fromRecord |> Json.fromJSObject |> Json.toJSString " " |> JS.toString
  
-toRequestData (x,y) = {x = x, y = y} |> serialize
+toRequestData (t, (x,y)) = {t = t, x = x, y = y} |> serialize
  
 toRequest event = case event of 
-  (x,y) -> firebaseRequest "post" (event |> toRequestData)
+  (t, (x,y)) -> firebaseRequest "post" (event |> toRequestData)
  
 requests = clicks ~> toRequest
  
